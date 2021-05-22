8aeba3b790896b6e809924e0f2c706d99121bcc8
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-22 15:39:18 -0400

    Create 07-oauth-open-authorisation.js

---
+++ 07-oauth-open-authorisation.js
@@ -0,0 +1,10 @@
+OAuth is an open standard for access delegation, commonly used as a way for Internet users to grant websites or applications access to their information on other websites but without giving them the passwords.
+
+authorization tokens to prove an identity between consumers and service providers
+
+able to acccess information from 3rd part website
+
+
+//https://www.passportjs.org/docs/
+//https://www.passportjs.org/packages/passport-google-oauth20/
+
